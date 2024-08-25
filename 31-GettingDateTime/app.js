9640763d5a42e88c5acf29662d7a4c28b0a94255
JanzaibYousaf <154719041+janzaibyousaf@users.noreply.github.com>
2024-08-25 04:30:04 -0700

    Add files via upload

---
+++ 31-GettingDateTime/app.js
@@ -0,0 +1,14 @@
+var date = new Date();
+console.log(date);
+
+
+var now = new Date();
+
+console.log(now.toDateString());
+
+
+var now = new Date();
+
+var arr = ["SUN","MON","TUE","WED","THU","FRI","SAT"];
+
+console.log(arr [date.getDay()]);
